dc477dbf3af2274189829db5f4dd94aefeda5324
Prakhar Gupta <mywolfdale@gmail.com>
2014-12-16 16:35:44 +0530

    Create hex-time.js

---
+++ hex-time.js
@@ -0,0 +1,17 @@
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    h= checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.body.style.backgroundColor="#"+h+""+m+""+s;
+    document.getElementById('txt').innerHTML =h+":"+m+":"+s;
+    var t = setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
